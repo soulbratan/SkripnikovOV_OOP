4f2160de02652fcc3b0d09e5e73ed9cdb3801537
Oleg Skripnikov <osv719@gmail.com>
2025-05-18 20:20:39 +0700

    create class Category

--- src/utils.py
+++ src/utils.py
@@ -12,3 +12,19 @@ class Product:
         self.description = description
         self.price = price
         self.quantity = quantity
+
+
+class Category:
+    """Класс категории товара для интернет-магазина"""
+    name: str
+    description: str
+    products: list
+    category_count: int = 0
+    product_count: int = 0
+
+    def __init__(self, name, description, products=None):
+        self.name = name
+        self.description = description
+        self.products = products if products else []
+        Category.category_count += 1
+        Category.product_count += len(products) if products else 0
